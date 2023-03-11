0f43336fe60af5878cc1fc75b211090e8bd77b2b
kait <kingkittykait@gmail.com>
2023-03-11 10:32:40 -0800

    ran install, made minor adjustments

--- root/routes/htmlRoutes.js
+++ root/routes/htmlRoutes.js
@@ -2,15 +2,15 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/notes', (reg, res) => {
-    res.sendFile(path.join(__dirname, '../public/note.html'));
+    res.sendFile(path.join(__dirname, '../root/note.html'));
 });
 
 router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../root/index.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../root/index.html'));
 });
 
 module.exports = router;
