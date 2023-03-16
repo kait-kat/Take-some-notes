f04ee1fd9d65ee53b08035dfcbb8818ae71bd560
kait <kingkittykait@gmail.com>
2023-03-16 10:26:17 -0700

    redid file structure and pathing, condensed files

--- routes/apiRoutes.js
+++
@@ -1,4 +0,0 @@
-const router = require('express').Router();
-const noteRoutes = require('./notesRoute.js');
-router.use(noteRoutes);
-module.exports = router;
--- server.js
+++ server.js
@@ -1,20 +1,92 @@
+//require all dependencies
 const express = require('express');
-const path = require('path'); 
-const htmlRoutes = require('./routes/htmlRoutes.js');
-const apiRoutes = require('./routes/apiRoutes.js');
-
-
+const path = require('path');
+const fs = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3001; 
-
-app.use(express.json()); 
-app.use(express.urlencoded({ extended:true })); 
-app.use(express.static(path.join(__dirname, '/assets'))); 
-
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+const PORT = process.env.PORT || 3001;
 
 
-app.listen(PORT, () => {
-    console.log('API server now on PORT: ${PORT}'); 
+app.use(express.static(__dirname + '/public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"))
 });
+app.get("/notes", (req, res) => {
+   res.sendFile(path.join(__dirname, "./public/notes.html"))
+});
+
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+      if (error) {
+          return console.log(error)
+      }
+      res.json(JSON.parse(notes))
+  })
+});
+
+
+app.post("/api/notes", (req, res) => {
+    let currentNote = req.body;
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error, notes) => {
+      if (error) {
+          return console.log(error)
+      }
+      notes = JSON.parse(notes)
+      if (notes.length > 0) {
+      let lastId = notes[notes.length - 1].id
+      const id =  parseInt(lastId)+ 1
+      } else {
+        const id = 10;
+      }
+  
+      let newNote = { 
+        title: currentNote.title, 
+        text: currentNote.text, 
+        id: currentNote.id
+        }
+     
+      const newNotesArr = notes.concat(newNote)
+      fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(newNotesArr), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(newNotesArr)
+        res.json(newNotesArr);
+      })
+  });
+ 
+});
+
+
+app.delete("/api/notes/:id", (req, res) => {
+	let deleteId = JSON.parse(req.params.id);
+	console.log("ID to be deleted: " ,deleteId);
+		fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+		  if (error) {
+		  return console.log(error)
+		  }
+		 let notesArray = JSON.parse(notes);
+   for (const i=0; i<notesArray.length; i++){
+     if(deleteId == notesArray[i].id) {
+       notesArray.splice(i,1);
+
+       fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(notesArray), (error, data) => {
+        if (error) {
+          return error
+        }
+        console.log(notesArray)
+        res.json(notesArray);
+      })
+     }
+  }
+  
+}); 
+});
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
